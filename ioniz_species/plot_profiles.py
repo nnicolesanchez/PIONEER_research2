30ffbab5966b79adeb757a5d5c1846372149e462
Natalie N Sanchez <nnsanche@pfe26.nas.nasa.gov>
2018-04-30 00:46:57 -0700

    Profiles with all GMs

--- ioniz_species/plot_profiles.py
+++ ioniz_species/plot_profiles.py
@@ -6,11 +6,18 @@ labels = ['P0','GM1','GM4','GM5','GM6','GM7']
 colors = ['DodgerBlue','SteelBlue','FireBrick','IndianRed','Salmon','Orange']
 
 
-for k in range(len(labels)-1):
+
+
+for k in range(len(labels)):
+    if labels[k] == 'GM7':
+        time = '3968'
+    else:
+        time = '4096'
+
 #    T = np.loadtxt(labels[k]+'_T_z0.np')
 #    R = np.loadtxt(labels[k]+'_Rbins_z0.np')
-    T = np.loadtxt('T_thrutime/'+labels[k]+'_T_4096.np')
-    R = np.loadtxt('Rbins_thrutime/'+labels[k]+'_Rbins_4096.np')
+    T = np.loadtxt('T_thrutime/'+labels[k]+'_T_'+time+'.np')
+    R = np.loadtxt('Rbins_thrutime/'+labels[k]+'_Rbins_'+time+'.np')
     plt.plot(R,T,label=labels[k],color=colors[k])
 
 
@@ -24,9 +31,14 @@ plt.savefig('ALLGMs_T_R.pdf')
 plt.show()
 plt.close()
 
-for j in range(len(labels)-1):
-    Novi = np.loadtxt('Novi_thrutime/'+labels[j]+'_Novi_4096.np')
-    R = np.loadtxt('Rbins_thrutime/'+labels[j]+'_Rbins_4096.np')
+for j in range(len(labels)):
+    if labels[j] == 'GM7':
+        time = '3968'
+    else:
+        time = '4096'
+
+    Novi = np.loadtxt('Novi_thrutime/'+labels[j]+'_Novi_'+time+'.np')
+    R = np.loadtxt('Rbins_thrutime/'+labels[j]+'_Rbins_'+time+'.np')
     plt.plot(R,Novi,label=labels[j],color=colors[j])
 
 plt.title('z = 0.0')
@@ -39,9 +51,14 @@ plt.savefig('ALLGMs_Novi_R.pdf')
 plt.show()
 plt.close()
 
-for j in range(len(labels)-1):
-    M_ox = np.loadtxt('Omass_thrutime/'+labels[j]+'_Omass_4096.np')
-    R = np.loadtxt('Rbins_thrutime/'+labels[j]+'_Rbins_4096.np')
+for j in range(len(labels)):
+    if labels[j] == 'GM7':
+        time = '3968'
+    else:
+        time = '4096'
+
+    M_ox = np.loadtxt('Omass_thrutime/'+labels[j]+'_Omass_'+time+'.np')
+    R = np.loadtxt('Rbins_thrutime/'+labels[j]+'_Rbins_'+time+'.np')
     plt.plot(R,M_ox,label=labels[j],color=colors[j])
 
 plt.title('z = 0.0')
@@ -61,15 +78,20 @@ C_2 = 5 * 10**7 #K
 f_m = 1.0 # metallicity dependent constant is 1 for solar metallicity Balogh et al 2013
 mu = 0.6  # solar metallicity
 
-for j in range(len(labels)-1):
-    rho = np.loadtxt('rho_thrutime/'+labels[j]+'_rho_4096.np')
-    R = np.loadtxt('Rbins_thrutime/'+labels[j]+'_Rbins_4096.np')
-    T = np.loadtxt('T_thrutime/'+labels[k]+'_T_4096.np')
+for i in range(len(labels)):
+    if labels[i] == 'GM7':
+        time = '3968'
+    else:
+        time = '4096'
+
+    rho = np.loadtxt('rho_thrutime/'+labels[i]+'_rho_'+time+'.np')
+    R = np.loadtxt('Rbins_thrutime/'+labels[i]+'_Rbins_'+time+'.np')
+    T = np.loadtxt('T_thrutime/'+labels[i]+'_T_'+time+'.np')
     t_cool = (C_1*mu*m_H*(10**T)**(0.5))/((10**rho)*(1+(C_2*f_m/(10**T)))) / (3.154 * 10**7)# years
-    plt.plot(R,np.log10(t_cool),label=labels[j],color=colors[j])
+    plt.plot(R,np.log10(t_cool),label=labels[i],color=colors[i])
 
 plt.title('z = 0.0')
-plt.ylabel(r't$_{cool}$ [years]')
+plt.ylabel(r'log(t$_{cool}$) [years]')
 plt.xlabel('R [kpc]')
 #plt.ylim(-0.05,1.2)
 plt.xlim(-10,260)
