83e670616b85e22201628410b6146730983d9626
Natalie N Sanchez <nnsanche@pfe21.nas.nasa.gov>
2018-07-31 09:50:38 -0700

    Changed Rkpc color bar to over Rvir

--- properties/CGMphasediagram.py
+++ properties/CGMphasediagram.py
@@ -20,23 +20,25 @@ import sys
 #plt.rc('lines', lw=2)
 #plt.rc('axes', lw=1, labelsize=12)
 
+time = '3456'
+#time = '1739'
 if len(sys.argv) == 1:
     print('No galaxy selected. Current options: P0, GM1, GM4, GM5, GM6')
     print('Syntax: "GM_xygasplots.py GM1"')
     quit()
 else:
     if (str(sys.argv[1]) == 'P0'):
-        sim = pynbody.load('/nobackupp2/nnsanche/pioneer50h243.1536g1bwK1BH/pioneer50h243.1536gst1bwK1BH.003456')
+        sim = pynbody.load('/nobackupp2/nnsanche/pioneer50h243.1536g1bwK1BH/pioneer50h243.1536gst1bwK1BH.00'+time)
     elif (str(sys.argv[1]) == 'GM1'):
-        sim = pynbody.load('/nobackupp2/nnsanche/pioneer50h243GM1.1536gst1bwK1BH/pioneer50h243GM1.1536gst1bwK1BH.003456')
+        sim = pynbody.load('/nobackupp2/nnsanche/pioneer50h243GM1.1536gst1bwK1BH/pioneer50h243GM1.1536gst1bwK1BH.00'+time)
     elif (str(sys.argv[1]) == 'GM4'):
-        sim = pynbody.load('/nobackupp2/nnsanche/pioneer50h243GM4.1536gst1bwK1BH/pioneer50h243GM4.1536gst1bwK1BH.003456')    
+        sim = pynbody.load('/nobackupp2/nnsanche/pioneer50h243GM4.1536gst1bwK1BH/pioneer50h243GM4.1536gst1bwK1BH.00'+time)    
     elif (str(sys.argv[1]) == 'GM5'):
-        sim = pynbody.load('/nobackup/nnsanche/pioneer50h243GM5.1536gst1bwK1BH/pioneer50h243GM5.1536gst1bwK1BH.003546')
+        sim = pynbody.load('/nobackup/nnsanche/pioneer50h243GM5.1536gst1bwK1BH/pioneer50h243GM5.1536gst1bwK1BH.00'+time)
     elif (str(sys.argv[1]) == 'GM6'):
-        sim = pynbody.load('/nobackupp8/fgoverna/pioneer50h243GM6.1536gst1bwK1BH/pioneer50h243GM6.1536gst1bwK1BH.003456')
+        sim = pynbody.load('/nobackupp8/fgoverna/pioneer50h243GM6.1536gst1bwK1BH/pioneer50h243GM6.1536gst1bwK1BH.00'+time)
     elif (str(sys.argv[1]) == 'GM7'):
-        sim = pynbody.load('/nobackupp2/nnsanche/pioneer50h243GM7.1536gst1bwK1BH/pioneer50h243GM7.1536gst1bwK1BH.003456') 
+        sim = pynbody.load('/nobackupp2/nnsanche/pioneer50h243GM7.1536gst1bwK1BH/pioneer50h243GM7.1536gst1bwK1BH.00'+time) 
 
     else :
         print('Not a valid option. Current options: P0, GM1, GM4, GM7')
@@ -80,7 +82,7 @@ x = np.log10(CGM_gas['rho'].in_units('g cm**-3')/m_H)
 y = np.log10(CGM_gas['temp'])
 #z = np.log10(CGM_gas['mass'].in_units('Msol'))
 #z = CGM_gas['metals']/Z_sun
-z = CGM_gas['r'].in_units('kpc')
+z = CGM_gas['r'].in_units('kpc')/int(CGM_gas['r'].max())
 #print(np.max(CGM_gas['mass'].in_units('Msol')),np.min(CGM_gas['mass'].in_units('Msol')))
 #print(CGM_gas['r'].min(),np.sort(CGM_gas['r']))
 #print(CGM_gas['mass'].units)
@@ -90,15 +92,15 @@ fig = plt.figure(figsize=(7, 5))
 
 #plt.hexbin(x,y,C=z,reduce_C_function=np.sum,cmap=cm.jet,mincnt=1,bins='log',vmin=1.25,vmax=3.75)
 #plt.hexbin(x,y,C=z,vmin=0.01,vmax=1)#mincnt=1,bins='log',vmin=1.25,vmax=3.75)
-plt.hexbin(x,y,C=z,cmap=cm.plasma,vmin=10,vmax=int(CGM_gas['r'].max()))
+plt.hexbin(x,y,C=z,cmap=cm.plasma,vmin=0.1,vmax=1)#vmin=10,vmax=270)#int(CGM_gas['r'].max()))
 
 plt.ylabel(r'Log$_{10}$ T ('+str(CGM_gas['temp'].units)+')',size=15)
 plt.xlabel(r'Log$_{10}$ n$_H$ (cm$^{-3}$)',size=15)
 #plt.colorbar(label=r'M$_{CGM}$/M$_{\odot}$')
 #plt.colorbar(label=(r'Z/Z$_{\odot}$'))
-plt.colorbar(label=(r'R [kpc]'))
+plt.colorbar(label=(r'R [kpc]/R$_{vir}$'))
 plt.text(-5.5,6.7,name, color='black',size=15)
-plt.text(0,6.7,'z = 0.17',color='black',size=15)
+plt.text(0,6.7,'z = '+str('%.2f' % sim.properties['z']),color='black',size=15)
 plt.xlim(-6,2)
 plt.ylim(3.5,7)
 #plt.savefig(name+'_phasediagram_mass.pdf')
