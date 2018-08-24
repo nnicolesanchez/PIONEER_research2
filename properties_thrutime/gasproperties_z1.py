645ab4a4c23b9fb7b88285ae85432f29e5f50bbe
Natalie N Sanchez <nnsanche@pfe27.nas.nasa.gov>
2018-08-24 14:34:56 -0700

    Trying to include missing particles

--- properties_thrutime/gasproperties_z1.py
+++ properties_thrutime/gasproperties_z1.py
@@ -14,15 +14,17 @@ import numpy as np
 import pynbody
 import sys
 
-timesteps = ['1280','1408','1536','1664','1739']
+timesteps = ['1280','1408','1536','1664','1739'] # two before and two after merger!
 sims = ['/nobackupp2/nnsanche/pioneer50h243.1536g1bwK1BH/pioneer50h243.1536gst1bwK1BH.00','/nobackupp2/nnsanche/pioneer50h243GM1.1536gst1bwK1BH/pioneer50h243GM1.1536gst1bwK1BH.00','/nobackupp2/nnsanche/pioneer50h243GM4.1536gst1bwK1BH/pioneer50h243GM4.1536gst1bwK1BH.00','/nobackupp2/nnsanche/pioneer50h243GM5.1536gst1bwK1BH/pioneer50h243GM5.1536gst1bwK1BH.00','/nobackupp2/nnsanche/pioneer50h243GM6.1536gst1bwK1BH/pioneer50h243GM6.1536gst1bwK1BH.00','/nobackupp2/nnsanche/pioneer50h243GM7.1536gst1bwK1BH/pioneer50h243GM7.1536gst1bwK1BH.00']
 labels = ['P0','GM1','GM4','GM5','GM6','GM7']
 
 for i in range(len(sims)-2):
+    #timesteps = np.loadtxt('../'+labels[i]+'/timesteps.txt',dtype='str')
     if i == 0 or i == 3:
         continue
     else:
         for j in range(len(timesteps)):
+            
             print('Making plot for',labels[i],' at ',timesteps[j])
             sim = pynbody.load(sims[i]+timesteps[j],verbose=True)
             sim.physical_units()
@@ -32,7 +34,7 @@ for i in range(len(sims)-2):
             
             pynbody.plot.sph.image(h1.g,qty='temp',width='20 kpc',vmin=0.1, vmax=10**6)
             plt.title(labels[i]+', z ='+timesteps[j])
-            plt.savefig(labels[i]+'_xy_temp_20kpc_'+timesteps[j]+'.pdf')
+            plt.savefig(labels[i]+'_xy_temp_20kpc_'+timesteps[j]+'_grp.pdf')
 #            plt.show()
     
 
