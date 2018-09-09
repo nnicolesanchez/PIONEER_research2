Did I fix the units on totgasmass and Omass?

noBH_analysis/plot_profiles.py
@@ -1,3 +1,5 @@
+import matplotlib 
+matplotlib.use('Agg')
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -40,6 +42,8 @@ plt.xlabel('R [kpc]',size=15)
 plt.ylim(13,15)
 plt.xlim(-10,260)
 plt.legend(ncol=2,loc=3,fontsize=15)
+#plt.show()
+#quit()
 plt.savefig('ALLGMs_plusnoBH_totgasmass_R.pdf')
 plt.show()
 plt.close()
@@ -94,7 +98,7 @@ plt.ylim(0,1)
 plt.xlim(-10,260)
 plt.legend(ncol=2,fontsize=15)
 plt.savefig('ALLGMs_plusnoBH_Z_R.pdf')
-plt.show()
+#plt.show()
 plt.close()
 
 
@@ -124,7 +128,7 @@ plt.ylim(5.2,6.2)
 plt.xlim(-10,260)
 plt.legend(ncol=2,fontsize=15)
 plt.savefig('ALLGMs_plusnoBH_T_R.pdf')
-plt.show()
+#plt.show()
 plt.close()
 
 solid = [0,0.1]
@@ -153,7 +157,7 @@ plt.ylim(12.5,16.5)
 plt.xlim(-10,260)
 plt.legend(ncol=2,fontsize=15)
 plt.savefig('ALLGMs_plusnoBH_Novi_R.pdf')
-plt.show()
+#plt.show()
 plt.close()
 
 solid = [0,0.1]
@@ -178,35 +182,39 @@ plt.ylim(-27,-24.5)
 plt.xlim(-10,260)
 plt.legend(fontsize=15,ncol=2)
 plt.savefig('ALLGMs_plusnoBH_rho_R.pdf')
-plt.show()
+#plt.show()
 plt.close()
 
-
-quit()
+solid = [0,0.1]
+dashed = [0,0.1]
+plt.plot(solid,dashed,color='Black',linestyle='-',label='BH')
+plt.plot(solid,dashed,color='Black',linestyle='--',label='NO BH')
 for j in range(len(labels)):
     M_ox = np.loadtxt('../ioniz_species/Omass_thrutime/'+labels[j]+'_Omass_'+time+'.np')
-    M_ox = np.log10((10**M_ox)*16*m_p/(5.97*10**27))
+    M_ox = M_ox#*16*m_p/(5.97*10**27))
     R = np.loadtxt('../ioniz_species/Rbins_thrutime/'+labels[j]+'_Rbins_'+time+'.np')
     plt.plot(R,M_ox,label=labels[j],color=colors[j])
 
-Mox_noBH = np.loadtxt('GM4noBHs_Omass_3456.np')
-Mox_noBH = np.log10((10**Mox_noBH)*16*m_p/(5.97*10**27))
-R_noBH = np.loadtxt('GM4noBHs_Rbins_3456.np')
-plt.plot(R_noBH,Mox_noBH,label='GM4_noBH',color='Green',linestyle='--')
+for j in range(len(labels_noBHs)):
+    print('Read in: ',labels_noBHs[j])
+    noBH_Mox = np.loadtxt(labels_noBHs[j]+'_Omass_'+time+'.np')
+    noBH_Mox = noBH_Mox#*16*m_p/(5.97*10**27))
+    noBH_R = np.loadtxt(labels_noBHs[j]+'_Rbins_'+time+'.np')
+    plt.plot(noBH_R,noBH_Mox,color=colors_noBHs[j],linestyle=noBHline)
 
 plt.title('z = 0.17')
 plt.ylabel(r'log(M$_{O}$) [M$_{\odot}$]')
 plt.xlabel('R [kpc]')
-#plt.ylim(12.5,16.5)
+#plt.ylim(8,12.5)
 plt.xlim(-10,260)
 plt.legend()
-plt.savefig('ALLGMs_Omass_R_plusGM4noBH.pdf')
+plt.savefig('ALLGMs_plusnoBH_Omass_R.pdf')
 plt.show()
 plt.close()
 
 
 
-#quit()
+quit()
 # Calculate cooling time
 m_H = 1.67 * 10**-24 #g
 C_1 = 3.88 * 10**11 # s K^-1/2 cm^-3