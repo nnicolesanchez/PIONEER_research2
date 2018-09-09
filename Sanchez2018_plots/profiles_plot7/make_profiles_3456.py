Fixing Omass units

Sanchez2018_plots/profiles_plot7/make_profiles_3456.py
@@ -50,7 +50,7 @@ else:
     print('Invalid option. Goodbye.')
     quit()
 
-sims      = ['/nobackupp2/nnsanche/pioneer50h243.1536g1bwK1BH/pioneer50h243.1536gst1bwK1BH.004096','/nobackupp2/nnsanche/pioneer50h243GM1.1536gst1bwK1BH/pioneer50h243GM1.1536gst1bwK1BH.004096','/nobackupp2/nnsanche/pioneer50h243GM7.1536gst1bwK1BH/pioneer50h243GM7.1536gst1bwK1BH.003968','/nobackupp2/nnsanche/pioneer50h243GM4.1536gst1bwK1BH/pioneer50h243GM4.1536gst1bwK1BH.004096','/nobackup/nnsanche/NO_BHs/pioneer50h243.1536gst1bwK1/pioneer50h243.1536gst1bwK1.003456','/nobackup/nnsanche/NO_BHs/pioneer50h243GM1.1536gst1bwK1/pioneer50h243GM1.1536gst1bwK1.003456','/nobackup/nnsanche/NO_BHs/pioneer50h243GM7.1536gst1bwK1/pioneer50h243GM7.1536gst1bwK1.003456','/nobackup/nnsanche/NO_BHs/pioneer50h243GM4.1536gst1bwK1/pioneer50h243GM4.1536gst1bwK1.003456']
+sims      = ['/nobackupp2/nnsanche/pioneer50h243.1536g1bwK1BH/pioneer50h243.1536gst1bwK1BH.003456','/nobackupp2/nnsanche/pioneer50h243GM1.1536gst1bwK1BH/pioneer50h243GM1.1536gst1bwK1BH.003456','/nobackupp2/nnsanche/pioneer50h243GM7.1536gst1bwK1BH/pioneer50h243GM7.1536gst1bwK1BH.003456','/nobackupp2/nnsanche/pioneer50h243GM4.1536gst1bwK1BH/pioneer50h243GM4.1536gst1bwK1BH.003456','/nobackup/nnsanche/NO_BHs/pioneer50h243.1536gst1bwK1_3456/pioneer50h243.1536gst1bwK1.003456','/nobackup/nnsanche/NO_BHs/pioneer50h243GM1.1536gst1bwK1_3456/pioneer50h243GM1.1536gst1bwK1.003456','/nobackup/nnsanche/NO_BHs/pioneer50h243GM7.1536gst1bwK1_3456/pioneer50h243GM7.1536gst1bwK1.003456','/nobackup/nnsanche/NO_BHs/pioneer50h243GM4.1536gst1bwK1_3456/pioneer50h243GM4.1536gst1bwK1.003456']
 
 ion_labels = ['oi','oii','oiii','oiv','ov','ovi','ovii','oviii']
 nion       = [1,2,3,4,5,6,7,8]
@@ -78,7 +78,7 @@ for j in range(len(ion_labels)):
 #    CGM_gas[ion_labels[j]] = hdf5_ion_frac(CGM_gas,ion=ion_labels[j])
     CGM_gas[ion_labels[j]] = hdf5_ion_frac(CGM_gas,ion=ion_labels[j]) 
    
-CGMprofile = profile.Profile(CGM_gas,min='0.1 kpc',max='250 kpc')
+CGMprofile = profile.Profile(CGM_gas,min='10 kpc',max='250 kpc')
 
 for k in range(len(ion_labels)):
     np.savetxt(lab+'/'+lab+'_N'+ion_labels[k]+'_3456.np',np.log10((CGMprofile['mass'].in_units('g')*CGMprofile['OxMassFrac']*CGMprofile[ion_labels[k]]/(16*m_p))/CGMprofile._binsize.in_units('cm**2')))
@@ -89,4 +89,4 @@ np.savetxt(lab+'/'+lab+'_rho_3456.np',np.log10(CGMprofile['rho'].in_units('g cm^
 np.savetxt(lab+'/'+lab+'_Rbins_3456.np',CGMprofile['rbins'].in_units('kpc'))
 np.savetxt(lab+'/'+lab+'_totgasmass_3456.np',CGMprofile['mass'].in_units('Msol'))
 np.savetxt(lab+'/'+lab+'_Z_3456.np',CGMprofile['metals'])
-np.savetxt(lab+'/'+lab+'_Omass_3456.np',CGMprofile['OxMassFrac']*CGMprofile['mass'].in_units('g')) # in units g
+np.savetxt(lab+'/'+lab+'_Omass_3456.np',CGMprofile['OxMassFrac']*CGMprofile['mass'].in_units('Msol')) # in units Msol

Sanchez2018_plots/profiles_plot7/plot_profiles.py
@@ -47,6 +47,36 @@ plt.savefig('ALLGMs_plusnoBH_totgasmass_R.pdf')
 plt.show()
 plt.close()
 
+
+solid = [0,0.1]
+dashed = [0,0.1]
+plt.plot(solid,dashed,color='Black',linestyle='-',label='BH')
+plt.plot(solid,dashed,color='Black',linestyle='--',label='NO BH')
+for k in range(len(labels)):
+    print('Read in: ',labels[k])
+    Omass = np.loadtxt(labels[k]+'/'+labels[k]+'_Omass_'+time+'.np')
+    Omass = totgasmass # Converted arrays to Msun /(5.976*10**27) #solar mass                                                  
+    R = np.loadtxt(labels[k]+'/'+labels[k]+'_Rbins_'+time+'.np')
+    plt.plot(R,np.log10(Omass),label=NEW_lab[k],color=colors[k])
+
+for j in range(len(labels_noBHs)):
+    print('Read in: ',labels_noBHs[j])
+    noBH_Omass = np.loadtxt(labels_noBHs[j]+'/'+labels_noBHs[j]+'_Omass_'+time+'.np')
+    noBH_Omass = noBH_Omass# Converted arrays to Msun /(5.976*10**27) #solar mass                                         
+    noBH_R = np.loadtxt(labels_noBHs[j]+'/'+labels_noBHs[j]+'_Rbins_'+time+'.np')
+    plt.plot(noBH_R,np.log10(noBH_Omass),color=colors_noBHs[j],linestyle=noBHline)
+
+plt.title('z = 0.17')
+plt.ylabel('log(M$_{oxygen gas}$) [M$_{\odot}$]',size=15)
+plt.xlabel('R [kpc]',size=15)
+#plt.ylim(7.6,9.6)
+plt.xlim(-10,260)
+#plt.legend(ncol=2,loc=3,fontsize=15)                                                                                              
+plt.savefig('ALLGMs_plusnoBH_Omass_R.pdf')
+plt.show()
+plt.close()
+
+
 #solid = [-0.05,-0.1]
 #dashed = [-0.05,-0.1]
 #plt.plot(solid,dashed,color='Black',linestyle='-',label='BH')
@@ -111,29 +141,19 @@ dashed = [0,0.1]
 plt.plot(solid,dashed,color='Black',linestyle='-',label='BH')
 plt.plot(solid,dashed,color='Black',linestyle='--',label='NO BH')
 for j in range(len(labels)):
-    Novi = np.loadtxt('/home1/nnsanche/PIONEER_research2/ioniz_species/'+labels[j]+'_Novi_'+time+'_hdf5.np')
-    R = np.loadtxt('/home1/nnsanche/PIONEER_research2/ioniz_species/'+labels[j]+'_Rbins_'+time+'.np')
+    Novi = np.loadtxt(labels[k]+'/'+labels[k]+'_Novi_'+time+'.np')
+    R = np.loadtxt(labels[k]+'/'+labels[k]+'_Rbins_'+time+'.np')
+#    Novi = np.loadtxt('/home1/nnsanche/PIONEER_research2/ioniz_species/'+labels[j]+'_Novi_'+time+'_hdf5.np')
+#    R = np.loadtxt('/home1/nnsanche/PIONEER_research2/ioniz_species/'+labels[j]+'_Rbins_'+time+'.np')
     plt.plot(R,Novi,label=NEW_lab[j],color=colors[j])
 
 for j in range(len(labels_noBHs)):
     print('Read in: ',labels_noBHs[j])
-    noBH_Novi = np.loadtxt('/home1/nnsanche/PIONEER_research2/noBH_analysis/'+NEW_lab_noBHs[j]+'_Novi_'+time+'_hdf5.np')
-    noBH_R = np.loadtxt('/home1/nnsanche/PIONEER_research2/noBH_analysis/'+NEW_lab_noBHs[j]+'_Rbins_'+time+'.np')
+    noBH_Novi = np.loadtxt(labels_noBHs[j]+'/'+labels_noBHs[j]+'_Novi_'+time+'.np')
+    noBH_R = np.loadtxt(labels_noBHs[j]+'/'+labels_noBHs[j]+'_Rbins_'+time+'.np')
+#    noBH_Novi = np.loadtxt('/home1/nnsanche/PIONEER_research2/noBH_analysis/'+NEW_lab_noBHs[j]+'_Novi_'+time+'_hdf5.np')
+#    noBH_R = np.loadtxt('/home1/nnsanche/PIONEER_research2/noBH_analysis/'+NEW_lab_noBHs[j]+'_Rbins_'+time+'.np')
     plt.plot(noBH_R,noBH_Novi,color=colors_noBHs[j],linestyle=noBHline)
-
-
-# TURN ON FOR HDF5 DATA 
-#for j in range(len(labels)):
-#    Novi = np.loadtxt(labels[j]+'/hdf5_Nion/'+labels[j]+'_Novi_'+time+'.np')
-#    R = np.loadtxt(labels[j]+'/hdf5_Nion/'+labels[j]+'_Rbins_'+time+'.np')
-#    plt.plot(R,Novi,label=NEW_lab[j],color=colors[j])
-
-#for j in range(len(labels_noBHs)):
-#    print('Read in: ',labels_noBHs[j])
-#    noBH_Novi = np.loadtxt(labels_noBHs[j]+'/hdf5_Nion/'+labels_noBHs[j]+'_Novi_'+time+'.np')
-#    noBH_R = np.loadtxt(labels_noBHs[j]+'/hdf5_Nion/'+labels_noBHs[j]+'_Rbins_'+time+'.np')
-#    plt.plot(noBH_R,noBH_Novi,color=colors_noBHs[j],linestyle=noBHline)
-
 
 COS = pd.read_csv('COSHALOdata.txt',comment='#',index_col=False,header=None)
 
@@ -180,6 +200,32 @@ plt.legend(ncol=2,fontsize=15)
 plt.savefig('ALLGMs_plusnoBH_Novi_R.pdf')
 plt.show()
 plt.close()
+
+#### NOVII PROFILE ####
+solid = [0,0.1]
+dashed = [0,0.1]
+plt.plot(solid,dashed,color='Black',linestyle='-',label='BH')
+plt.plot(solid,dashed,color='Black',linestyle='--',label='NO BH')
+for j in range(len(labels)):
+    Novii = np.loadtxt('/home1/nnsanche/PIONEER_research2/ioniz_species/'+labels[j]+'_Novii_'+time+'_hdf5.np')
+    R = np.loadtxt('/home1/nnsanche/PIONEER_research2/ioniz_species/'+labels[j]+'_Rbins_'+time+'.np')
+    plt.plot(R,Novii,label=NEW_lab[j],color=colors[j])
+
+for j in range(len(labels_noBHs)):
+    print('Read in: ',labels_noBHs[j])
+    noBH_Novii = np.loadtxt('/home1/nnsanche/PIONEER_research2/noBH_analysis/'+NEW_lab_noBHs[j]+'_Novii_'+time+'_hdf5.np')
+    noBH_R = np.loadtxt('/home1/nnsanche/PIONEER_research2/noBH_analysis/'+NEW_lab_noBHs[j]+'_Rbins_'+time+'.np')
+    plt.plot(noBH_R,noBH_Novii,color=colors_noBHs[j],linestyle=noBHline)
+plt.title('z = 0.17')
+plt.ylabel(r'log(N$_{ovii}$) [cm$^{-2}$]',size=15)
+plt.xlabel('R [kpc]',size=14)
+#plt.ylim(12.5,16.5)
+plt.xlim(-10,260)
+plt.legend(ncol=2,fontsize=15)
+plt.savefig('ALLGMs_plusnoBH_Novii_R.pdf')
+plt.show()
+plt.close()
+
 
 solid = [0,0.1]
 dashed = [0,0.1]