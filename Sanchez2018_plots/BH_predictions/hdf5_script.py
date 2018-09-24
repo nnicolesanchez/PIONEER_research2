2fee17186e6e1c65a9ef5b54ea556a93a10df8a0
Natalie N Sanchez <nnsanche@pfe26.nas.nasa.gov>
2018-09-24 13:22:05 -0700

    New plots

---
+++ Sanchez2018_plots/BH_predictions/hdf5_script.py
@@ -0,0 +1,201 @@
+from pynbody.analysis import profile
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib as mpl
+import seaborn as sns
+import numpy as np
+import pynbody
+import h5py
+
+#from ..array import SimArray
+from pynbody import config
+import logging
+logger = logging.getLogger('pynbody.analysis.ionfrac')
+
+#from scipy.interpolate import interp3d
+from scipy.interpolate import RegularGridInterpolator as rgi
+
+from pynbody.analysis.interpolate import _interpolate3d
+
+def interpolate3d(x, y, z, x_vals, y_vals, z_vals, vals):
+    """
+    Interpolate on a 3D regular grid. 
+    Yields results identical to scipy.interpolate.interpn. 
+
+    Input
+    -----
+
+    x,y,z : points where the interpolation will be performed
+
+    x_vals, y_vals, z_vals : xyz values of the reference grid
+
+    vals : grid values
+    """
+
+    # cast x_vals, y_vals and z_vals to float64
+
+    x_vals = x_vals.astype(np.float64)
+    y_vals = y_vals.astype(np.float64)
+    z_vals = z_vals.astype(np.float64)
+    vals = vals.astype(np.float64)
+
+    result_array = np.empty(len(x), dtype=np.float64)
+
+    _interpolate3d.interpolate3d(len(x),
+                                 x, y, z,
+                                 len(x_vals), x_vals,
+                                 len(y_vals), y_vals,
+                                 len(z_vals), z_vals,
+                                 vals,
+                                 result_array)
+
+    return result_array
+
+def N_OVI(f):
+    ovi = pynbody.analysis.ionfrac_edit.calculate(f,ion='ovi',mode='new')
+    m_p = 1.6726 * 10**-24 #g
+    #print(ovi)
+    #print(f.gas['OxMassFrac'])
+    #print(f.gas['rho'].in_units('g cm**-3')*ovi*f.gas['OxMassFrac']/(16*m_p))
+    return f.gas['rho'].in_units('g cm**-3')*ovi*f.gas['OxMassFrac']/(16*m_p)
+
+def hdf5_ion_frac(sim, ion):
+    if ion == 'oi':
+        print('Loading OI')
+        nion = 0
+    elif ion == 'oii':
+        print('Loading OII')
+        nion = 1
+    elif ion == 'oiii':
+        print('Loading OIII')
+        nion = 2
+    elif ion == 'oiv':
+        print('Loading OIV')
+        nion = 3
+    elif ion == 'ov':
+        print('Loading OV')
+        nion = 4
+    elif ion == 'ovi':
+        print('Loading OVI')
+        nion = 5
+    elif ion == 'ovii':
+        print('Loading OVII')
+        nion = 6
+    elif ion == 'oviii':
+        print('Loading OVIII')
+        nion = 7
+    else:
+        print('Specified ion incompatible; Try again.')
+        
+    iffile = '/home1/nnsanche/hm2012_hr.h5'
+    ifs = h5py.File(iffile)
+    
+    x_vals = ifs['O'].attrs['Parameter1']   # HYDROGEN Densities
+    y_vals = ifs['O'].attrs['Parameter2']   # Redshifts
+    z_vals = ifs['O'].attrs['Temperature']  # Temperatures
+
+    vals = ifs['O'][nion]
+    y = np.zeros(len(sim.gas))
+    x = np.log10(sim.gas['rho'].in_units('m_p cm^-3')).view(np.ndarray)
+    y[:] = sim.properties['z']
+    y = y
+    z = np.log10(sim.gas['temp']).view(np.ndarray)
+ 
+    n = len(sim.gas)
+    n_z_vals = len(z_vals)
+    n_y_vals = len(y_vals)
+    n_x_vals = len(x_vals)
+    
+    # get values off grid to minmax                 
+    x[np.where(x < np.min(x_vals))] = np.min(x_vals)
+    x[np.where(x > np.max(x_vals))] = np.max(x_vals)
+    y[np.where(y < np.min(y_vals))] = np.min(y_vals)
+    y[np.where(y > np.max(y_vals))] = np.max(y_vals)
+    z[np.where(z < np.min(z_vals))] = np.min(z_vals)
+    z[np.where(z > np.max(z_vals))] = np.max(z_vals)
+    
+    # interpolate                              
+    result_array = interpolate3d(x, y, z, x_vals, y_vals, z_vals, vals)
+
+
+    return 10 ** result_array
+
+
+
+if __name__ == '__main__':
+
+    k = 0
+    sim = ['/nobackupp2/nnsanche/pioneer50h243.1536g1bwK1BH/pioneer50h243.1536gst1bwK1BH.004096','/nobackupp2/nnsanche/pioneer50h243GM1.1536gst1bwK1BH/pioneer50h243GM1.1536gst1bwK1BH.004096','/nobackupp2/nnsanche/pioneer50h243GM4.1536gst1bwK1BH/pioneer50h243GM4.1536gst1bwK1BH.004096','/nobackup/nnsanche/pioneer50h243GM5.1536gst1bwK1BH/pioneer50h243GM5.1536gst1bwK1BH.004096','/nobackupp2/nnsanche/pioneer50h243GM6.1536gst1bwK1BH/pioneer50h243GM6.1536gst1bwK1BH.004096','/nobackup/nnsanche/pioneer50h243GM7.1536gst1bwK1BH/pioneer50h243GM7.1536gst1bwK1BH.004096']
+    labels = ['P0','GM1','GM4','GM5','GM6','GM7']
+    colors = sns.cubehelix_palette(8)
+    Z_sun = 0.0142 # (Asplund 2009; https://arxiv.org/pdf/0909.0948.pdf)
+    m_p = 1.6726 * 10**-24 #g
+    print('LOADING SIM:',labels[k])
+    
+    ######################
+    # READ IN SIMULATION #
+    ######################
+    f = pynbody.load(sim[k])
+    pynbody.analysis.halo.center(f.star)
+    f.physical_units()
+    h = f.halos()
+    h1 = h[1]
+    pynbody.analysis.angmom.faceon(h1)
+    
+    ###################
+    # ISOLATE CGM GAS #   
+    ###################
+    # Isolate and remove disk stars within radius 0-10 kpc & vertically 10 kpc 
+    r_max = 10  # kpc
+    twenty_kpc_incm = 6.171*(10**22)
+    #z_max = 10 #4 # kpc
+    
+    Rg_d = ((h1.g['x'].in_units('kpc'))**2. + (h1.g['y'].in_units('kpc'))**2. + (h1.g['z'].in_units('kpc'))**2.)**(0.5)
+    disk_gas_xyzmax =  (Rg_d < r_max)
+    #disk_gas_zmax  = (h1.g['z'].in_units('kpc') < z_max) & (h1.g['z'].in_units('kpc') > -z_max)
+    disk_gas_mask = disk_gas_xyzmax #& disk_gas_zmax
+    disk_gas = h1.g[disk_gas_mask] #& disk_gas_zmax]
+    CGM_gas  = h1.g[~disk_gas_mask]
+    
+    CGM_gas['ovi_npz'] = pynbody.analysis.ionfrac.calculate(CGM_gas,ion='ovi',mode='new') 
+    CGM_gas['ovi_hdf5'] = hdf5_ion_frac(CGM_gas,ion='ovi')
+#    quit()
+    
+    CGMprofile = profile.Profile(CGM_gas,min='0.1 kpc',max='250 kpc')
+    
+    # Column Densities
+    Novi_npz = np.log10((CGMprofile['mass'].in_units('g')*CGMprofile['OxMassFrac']*CGMprofile['ovi_npz']/(16*m_p))/CGMprofile._binsize.in_units('cm**2'))
+    Novi_hdf5 = np.log10((CGMprofile['mass'].in_units('g')*CGMprofile['OxMassFrac']*CGMprofile['ovi_hdf5']/(16*m_p))/CGMprofile._binsize.in_units('cm**2'))
+    
+    print('USING .NPZ DATA:')
+    #print('Total mass in OVI in CGM:', np.sum(CGM_gas['OxMassFrac']*CGM_gas['mass']*CGM_gas['ovi_npz']))
+    print('OVI fractions:',np.average(CGM_gas['ovi_npz']))
+    
+    print('USING HDF5 DATA:')
+    #print('Total mass in OVI in CGM:', np.sum(CGM_gas['OxMassFrac']*CGM_gas['mass']*ovi))
+    print('OVI fractions:',np.average(CGM_gas['ovi_hdf5']))
+    
+    plt.plot(CGMprofile['rbins'].in_units('kpc'),Novi_hdf5,color='Salmon')
+    plt.plot(CGMprofile['rbins'].in_units('kpc'),Novi_npz,color='SteelBlue')
+    
+    plt.title('z = 0.17')
+    plt.ylabel(r'log(N$_{ovi}$) [cm$^{-2}$]',size=15)
+    plt.xlabel('R [kpc]',size=15)
+    plt.ylim(12.5,17.5)
+    plt.xlim(-10,260)
+    plt.legend(ncol=2,fontsize=15)
+    #plt.savefig('ALLGMs_plusnoBH_Novi_R.pdf')
+    plt.show()
+    plt.close()
+    
+    
+    quit()
+    OVI = CGM_gas['rho'].in_units('g cm**-3')*ovi*CGM_gas['OxMassFrac']/(16*m_p)
+    print('Total mass in CGM:', np.sum(CGM_gas['mass']))
+    print('Total mass in metals:',np.sum(CGM_gas['mass']*CGM_gas['metals'])) # 'metals' *IS* metallicity
+    print('Total mass Oxygen in CGM:', np.sum(CGM_gas['OxMassFrac']*CGM_gas['mass']),CGM_gas['mass'].units)
+    print('Total mass in OVI in CGM:', np.sum(CGM_gas['OxMassFrac']*CGM_gas['mass']*ovi))
+    #print('OVI Density: ',OVI,OVI.units)
+    print('OVI fractions:',np.average(ovi))
+    
+    quit()
--- Sanchez2018_plots/BH_predictions/plot_profiles.py
+++ Sanchez2018_plots/BH_predictions/plot_profiles.py
@@ -1,17 +1,72 @@
 #import matplotlib
 #matplotlib.use('Agg')
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from hdf5_script import *
+import sys
 
 #catalogue = 'grp'
 catalogue = 'AHF'
 
-### IDs for galaxies with stellar masses within COS-Halos range
-M_blue = [9,14,15,16,19,20,21,22,27,28,29,30,31,32,34,38,41,42,43,44,46,47,48,51,55,56,60,61,62,64,65,66,71,73,75,77,79,81,82,92] #lost 12,74 at 6912 
-M_red  = [10,11,17,18,23,24,26,33,35,36,37,45,49,50,58,70] 
-halo = M_blue + M_red
+HALO_data = pd.read_csv('/home1/nnsanche/ROMULUS_research/OVI_columndensities/MWmass_data/ROMULUS_highmass_info_ALL.csv',header=0)
+
+if sys.argv[1] == 'COS':
+    #### COS-HALOS Range
+    print('Plotting COS-Halos stellas mass range of ROM galaxies')    
+    M_blue = [9,10,14,15,16,17,18,19,20,21,22,26,27,28,29,30,31,32,33,34,35,36,37,38,42,43,44,45,46,47,48,49,50,51,58,61,63,68,69,70,72,77,79,80,87,89] #no 83,86
+    M_red  = [11,23,24,41,52,66]
+    halo   = M_blue + M_red
+    therange = 'COShalos'
+    if sys.argv[1] == 'halo':
+
+        ctype = 'Mhalo'
+        logmax = 12.8 #halo
+        logmin = 11.5 #halo
+        cblabel = r'M$_{halo}$'
+    else:
+
+        ctype = 'Mstar'
+        logmax = 11.4 #star
+        logmin = 10.5 #star
+        cblabel = r'M$_{star}$'
+elif sys.argv[1] == 'MW':
+    #### MW mass Range from Michael
+    halo = np.arange(25,56,1)
+    therange = 'MW'
+    if sys.argv[1] == 'halo':
+
+        ctype = 'Mhalo'
+        logmax = 12.2 #halo
+        logmin = 11.9 #halo
+        cblabel = r'M$_{halo}$'
+    else:
+        ctype = 'Mstar'
+        logmax = 11.1 #star
+        logmin = 10.2 # star
+        cblabel = r'M$_{star}$'
+
+else:
+    #### GM stellar mass range (10^10 - 5^10)
+    halo = [72,54,87,66,77,79,68,37,69,61,80,70,86,89,72,54,87,75,78,96,71,94,91,73,65,67,88,85,81,39,90,60,98,93,62,84,92,95,56,97]
+    therange = 'GMstellarrange'
+    if sys.argv[1] == 'halo':
+
+        ctype = 'Mvir'
+        logmax = 12.2 #halo
+        logmin = 11.5 #halo
+        cblabel = 'M$_{halo}$'
+
+    else:
+        ctype = 'Mstar'
+        logmax = 10.7 #star
+        logmin = 10 #star
+        cblabel = 'M$_{star}$'
+
 labels = map(str, halo)
 labels = list(labels)
+
+ROMcmap = cm.get_cmap('rainbow')
 
 # MUST HAVE TANGOS DATABASE POINTING AT ROM25 FOR THIS APRT
 import tangos
@@ -22,29 +77,40 @@ ROM_BH_masses = []
 ROM_BH_ids = []
 for i in range(len(halo)):
     ROM_h = tangos.get_halo('cosmo25/cosmo25p.768sg1bwK1BHe75.006912/halo_'+labels[i])
-    ROM_BHmass = ROM_h['BH_central'][0]['BH_mass']
-    ROM_BH_masses.append(ROM_BHmass)
-    ROM_BH_ids.append(halo[i])
-    print('Halo ',halo[i],'has a Mvir =',ROM_h['Mvir'],' Mstar =',ROM_h['Mstar'],', Rvir =',ROM_h['max_radius'])
+    try:
+        ROM_BHmass = ROM_h['BH_central'][0]['BH_mass']
+        ROM_BH_masses.append(ROM_BHmass)
+        ROM_BH_ids.append(halo[i])
+        print('Halo ',halo[i],'has a Mvir =',ROM_h['Mvir'],' Mstar =',ROM_h['Mstar'],', Rvir =',ROM_h['max_radius'])
+        if np.log10(ROM_h['Mstar']) < 10:
+            print('M_star too LOW')
+            continue
 
-    np.savetxt('ROM_data_forhalos/ROM_BH_masses.txt',ROM_BH_masses)
-    np.savetxt('ROM_data_forhalos/ROM_BH_ids.txt',ROM_BH_ids)
+        np.savetxt('ROM_data_forhalos/ROM_BH_masses.txt',ROM_BH_masses)
+        np.savetxt('ROM_data_forhalos/ROM_BH_ids.txt',ROM_BH_ids)
 
-    ROM_Novi = np.loadtxt('/home1/nnsanche/ROMULUS_research/OVI_columndensities/COS_halos_range_data/Novi_profile_np/'+str(halo[i])+'_Novi_profile_Novi_6912.np',dtype=float)
+        ROM_Novi = np.loadtxt('/home1/nnsanche/ROMULUS_research/OVI_columndensities/COS_halos_range_data/Novi_profile_np/'+str(halo[i])+'_Novi_profile_Novi_6912.np',dtype=float)
+    except:
+        print('NO NOVI')
+        continue
     ROM_Rbins = np.loadtxt('/home1/nnsanche/ROMULUS_research/OVI_columndensities/COS_halos_range_data/Novi_profile_np/'+str(halo[i])+'_Novi_profile_rbins_6912.np',dtype=float)
 
-#    print(ROM_Rbins)
+
+    colors = ROMcmap((np.log10(ROM_h[ctype])-logmin)/(logmax-logmin))
+
     SFR_at25Myr = ROM_h['SFR_encl_25Myr']/(0.6*ROM_h['Mstar'])
-    if halo[i] in M_red:#SFR_at25Myr[-1:] < 1.6*10**-11 :
+#    if halo[i] in M_red:#SFR_at25Myr[-1:] < 1.6*10**-11 :
 #        SFR = ROM_h['inner_SFR_histogram']
 #        SFR_property_object = ROM_h.get_objects('SFR_histogram')[0]
 #        SFR_time_bins = SFR_property_object.x_values()
 #        p.plot(SFR_time_bins, SFR)    
-        print('THIS GALAXY IS RED')        
-        plt.plot(np.log10(ROM_BHmass),np.log10(ROM_Novi[ROM_Rbins == 76.3195]),color='DarkGrey',alpha=0.5,marker='s',markersize=8)
-    else:
-        plt.plot(np.log10(ROM_BHmass),np.log10(ROM_Novi[ROM_Rbins == 76.3195]),color='DarkGrey',alpha=0.5,marker='o',markersize=8)
+#        print('THIS GALAXY IS RED')        
+#        plt.plot(np.log10(ROM_BHmass),np.log10(ROM_Novi[ROM_Rbins == 76.3195]),color=colors,alpha=0.5,marker='s',markersize=8)
+#    else:
+    plt.plot(np.log10(ROM_BHmass),np.log10(ROM_Novi[ROM_Rbins == 76.3195]),alpha=0.5,marker='o',markersize=8,color=colors)
 
+sm = plt.cm.ScalarMappable(cmap=ROMcmap, norm=plt.Normalize(vmin=logmax, vmax=logmin))
+sm._A = []
 
 labels = ['P0','GM1','GM7','GM4']
 NEW_lab = ['P0','GM1','GM2','GM3']
@@ -56,7 +122,7 @@ spot = ['o','o','s','s']
 
 time = '3456'
 
-plt.plot([0],[1],marker='o',linestyle=None,label='Star Forming',color='Black')
+plt.plot([0],[1],marker='o',linestyle=None,label='SF',color='Black')
 plt.plot([0],[1],marker='s',linestyle=None,label='Passive',color='Black')
 for k in range(len(labels)):
     print('Read in: ',labels[k])
@@ -66,15 +132,16 @@ for k in range(len(labels)):
     BH_times = np.loadtxt('/home1/nnsanche/PIONEER_research2/BH_studies/'+NEW_lab[k]+'_BHtime.txt')
     BH_mass  = np.loadtxt('/home1/nnsanche/PIONEER_research2/BH_studies/'+NEW_lab[k]+'_BHmass.txt')
 
-    plt.plot(np.log10(BH_mass[(BH_times > 11.645) & (BH_times < 11.65)][0]),Novi[R == 76.],label=NEW_lab[k],color=colors[k],marker=spot[k],markersize=8,linestyle=None)
+    plt.plot(np.log10(BH_mass[(BH_times > 11.645) & (BH_times < 11.65)][0]),Novi[R == 76.],label=NEW_lab[k],color=colors[k],marker=spot[k],markersize=8,linestyle=None,markeredgecolor='Black')
 
 plt.text(6.75,15.25,'z = 0.17',size=15)
 plt.ylabel(r'N$_{Ovi}$ at 75 kpc',size=15)
 plt.xlabel(r'M$_{BH}$/M$_{\odot}$',size=15)
-plt.ylim(13.,15.5)
-plt.xlim(6.6,9.5)
-plt.legend(fontsize=12)#,loc=4)
-plt.savefig('BHmass_Noviat75kpc.pdf')
+plt.ylim(10.,15.5)
+plt.xlim(5,9.5)
+plt.legend(fontsize=11,ncol=1,loc=1)
+plt.colorbar(sm,label=r'log '+cblabel+'/M$_{\odot}$')
+plt.savefig('BHmass_Noviat75kpc_'+therange+'_color'+ctype+'.pdf')
 plt.show()
 
 
