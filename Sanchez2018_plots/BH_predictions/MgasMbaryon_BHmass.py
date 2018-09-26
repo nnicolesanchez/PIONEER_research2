ba3a9328d6d7dcaca29e04cf4e98db98888b907f
Natalie N Sanchez <nnsanche@pfe23.nas.nasa.gov>
2018-09-26 13:20:32 -0700

    Added colobar by Mstellar

--- Sanchez2018_plots/BH_predictions/MgasMbaryon_BHmass.py
+++ Sanchez2018_plots/BH_predictions/MgasMbaryon_BHmass.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
+from scipy.stats import *
 
-Mgas = np.loadtxt('COSstellarmass_range_data/ROM_Mtotgas_10_100kpc_z017.txt')
-Mmetals = np.loadtxt('COSstellarmass_range_data/ROM_Mtotmetals_10_100kpc_z017.txt')
-Mhalo = np.loadtxt('COSstellarmass_range_data/ROM_halomasses_z017.txt')
+Mgas     = np.loadtxt('COSstellarmass_range_data/ROM_Mtotgas_10_100kpc_z017.txt')
+Mmetals  = np.loadtxt('COSstellarmass_range_data/ROM_Mtotmetals_10_100kpc_z017.txt')
+Mhalo    = np.loadtxt('COSstellarmass_range_data/ROM_halomasses_z017.txt')
+Mstellar = np.loadtxt('COSstellarmass_range_data/ROM_stellarmass_z017.txt')
 
 BHmass = np.loadtxt('ROM_data_forhalos/ROM_BH_masses.txt')
 
@@ -11,16 +14,15 @@ Mbaryon         = Mhalo*0.17
 Mgas_Mbaryon    = Mgas/Mbaryon
 Mmetals_Mbaryon = Mmetals/Mbaryon
 
-# Plot Mgas/Mbaryon vs BHmass
-plt.plot(np.log10(BHmass),Mgas_Mbaryon,marker='o',linestyle=' ',color='Black',alpha=0.7)
-plt.ylabel(r'M$_{gas}$/M$_{baryon}$')
-plt.xlabel(r'M$_{BH}$/M$_{\odot}$')
-plt.savefig('Mgasoverbaryons_BHmass.pdf')
-plt.show()
+rho, p_val = spearmanr(BHmass,Mgas)
+print(rho,p_val)
 
-plt.plot(np.log10(BHmass),Mmetals_Mbaryon,marker='o',linestyle=' ',color='Black',alpha=0.7)
-plt.ylabel(r'M$_{metals}$/M$_{baryon}$')
-plt.xlabel(r'M$_{BH}$/M$_{\odot}$')
-plt.savefig('Mmetalsoverbaryons_BHmass.pdf')
+# Plot Mgas/Mbaryon vs BHmass
+sm = plt.scatter(np.log10(BHmass),Mgas_Mbaryon,marker='o',alpha=0.7,cmap='rainbow',c=np.log10(Mstellar),vmin=10.5,vmax=11.5)
+
+plt.ylabel(r'M$_{gas}$/M$_{baryon}$',size=15)
+plt.xlabel(r'M$_{BH}$/M$_{\odot}$',size=15)
+plt.colorbar().set_label(label=r'log M$_{*}$/M$_{\odot}$',size=15)
+plt.savefig('Mgasoverbaryons_BHmass.pdf')
 plt.show()
 
